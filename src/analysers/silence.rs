4cf85d5627ea9d1a1b4ea5c4d5aa89d7f69cfe44
vonforum <vonforum@users.noreply.github.com>
2025-10-24 13:24:50 +0300

    Refactor analysis into separate modules

---
+++ src/analysers/silence.rs
@@ -0,0 +1,152 @@
+use ebur128::{EbuR128, Error as EbuR128Error, Mode};
+use wavers::{Samples, Wav};
+
+use super::Analyser;
+use crate::{debug, output, output::frame_to_time};
+
+#[derive(Debug, Clone)]
+pub struct SilenceState {
+    pub previous_lufs: f64,
+    pub silence_start_frame: usize,
+    pub silence_end_frame: usize,
+}
+
+impl SilenceState {
+    pub fn new() -> Self {
+        Self {
+            previous_lufs: 0.0,
+            silence_start_frame: 0,
+            silence_end_frame: 0,
+        }
+    }
+}
+
+pub struct SilenceAnalyser {
+    count: usize,
+    enabled: bool,
+    frame_buf: Vec<i32>,
+    frame_buf_iter: usize,
+    loudness: EbuR128,
+    lufs: f64,
+    num_frames: usize,
+    percentage: f32,
+    sample_rate: i32,
+    state: SilenceState,
+    window_size: usize,
+}
+
+impl SilenceAnalyser {
+    pub fn new(args: &crate::cli::Cli, wav: &Wav<i32>) -> Result<Self, EbuR128Error> {
+        let (_, spec) = wav.wav_spec();
+        let sample_rate = spec.fmt_chunk.sample_rate;
+        let loudness = EbuR128::new(
+            wav.n_channels().into(),
+            sample_rate as u32,
+            Mode::S | Mode::I,
+        )?;
+
+        let window_size = sample_rate as usize * wav.n_channels() as usize;
+
+        Ok(Self {
+            count: 0,
+            enabled: args.silence,
+            frame_buf: vec![0; window_size],
+            frame_buf_iter: 0,
+            loudness,
+            lufs: args.lufs,
+            num_frames: wav.n_samples(),
+            percentage: args.silence_percentage as f32,
+            sample_rate,
+            state: SilenceState::new(),
+            window_size,
+        })
+    }
+}
+
+impl Analyser for SilenceAnalyser {
+    fn analyse(&mut self, label: &str, frame_counter: usize, frame: &Samples<i32>) {
+        if !self.enabled {
+            return;
+        }
+
+        for sample in frame.iter() {
+            self.frame_buf[self.frame_buf_iter] = *sample;
+            self.frame_buf_iter += 1;
+        }
+
+        if self.frame_buf_iter >= self.window_size {
+            self.frame_buf_iter = 0;
+            self.loudness.reset();
+
+            if let Err(err) = self.loudness.add_frames_i32(&self.frame_buf) {
+                println!(
+                    "Warning: error adding frame to loudness measurement: {:?}",
+                    &err
+                );
+            }
+
+            let lufs = self
+                .loudness
+                .loudness_shortterm()
+                .unwrap_or(f64::NEG_INFINITY);
+            if lufs < self.lufs && self.state.previous_lufs >= self.lufs {
+                self.state.silence_start_frame = frame_counter;
+                output!(
+                    "[{}] SILENCE START: LUFS-S: {:04.3}; LUFS-I: {:04.3} @ {}",
+                    label,
+                    lufs,
+                    self.loudness.loudness_global().unwrap_or(-f64::INFINITY),
+                    frame_to_time(frame_counter, self.sample_rate)
+                );
+            }
+
+            if lufs >= self.lufs && self.state.previous_lufs < self.lufs {
+                self.state.silence_end_frame = frame_counter;
+                self.count += self.state.silence_end_frame - self.state.silence_start_frame;
+
+                output!(
+                    "[{}] SILENCE END  : LUFS-S: {:04.3}; LUFS-I: {:04.3} @ {} ({:04.3}% of total)",
+                    label,
+                    lufs,
+                    self.loudness.loudness_global().unwrap_or(-f64::INFINITY),
+                    frame_to_time(frame_counter, self.sample_rate),
+                    (self.count as f32 / self.num_frames as f32) * 100.0
+                );
+            }
+
+            self.state.previous_lufs = lufs;
+            debug!(
+                "[{}] DEBUG        : LUFS-S: {:04.3}; LUFS-I: {:04.3} @ {}",
+                label,
+                lufs,
+                self.loudness.loudness_global().unwrap_or(-f64::INFINITY),
+                frame_to_time(frame_counter, self.sample_rate)
+            );
+        }
+    }
+
+    fn finish(&self, label: &str) -> u8 {
+        if !self.enabled {
+            return 0;
+        }
+
+        if self.state.previous_lufs < self.lufs {
+            let end_frame = self.num_frames;
+            let count = self.count + end_frame - self.state.silence_start_frame;
+            output!(
+                "[{}] SILENCE END  : LUFS-S: {:04.3}; LUFS-I: {:04.3} @ {} ({:04.3}% of total)",
+                label,
+                self.state.previous_lufs,
+                self.loudness.loudness_global().unwrap_or(-f64::INFINITY),
+                frame_to_time(self.num_frames, self.sample_rate),
+                (count as f32 / self.num_frames as f32) * 100.0
+            );
+
+            if (count as f32 / self.num_frames as f32) * 100.0 >= self.percentage {
+                return crate::ERR_CONTAINS_SILENCE;
+            }
+        }
+
+        0
+    }
+}
